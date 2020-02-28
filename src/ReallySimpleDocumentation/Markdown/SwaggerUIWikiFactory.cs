e08087f576711924a526244da2666630e71554a6
Dan Mann <dan@marsman.co.uk>
2020-02-28 16:48:10 +0000

    keep tag/section references inline between swagger and redoc

--- src/ReallySimpleDocumentation/Markdown/SwaggerUIWikiFactory.cs
+++ src/ReallySimpleDocumentation/Markdown/SwaggerUIWikiFactory.cs
@@ -17,13 +17,30 @@ namespace Marsman.ReallySimpleDocumentation
     {
         private Lazy<string> wikiHtmlSection;
         private SwaggerUIWikiOptions options;
+        private readonly RedocUiOptions redocOptions;
         private readonly IWikiMarkdownHandler markdownHandler;
+        private readonly string idSlug;
 
-        public SwaggerUIWikiFactory(IWikiMarkdownHandler markdownHandler, IOptions<SwaggerUIWikiOptions> options)
+        public SwaggerUIWikiFactory(IWikiMarkdownHandler markdownHandler, IOptions<SwaggerUIWikiOptions> options, IOptions<RedocUiOptions> redocOptions)
         {
             this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            this.redocOptions = redocOptions?.Value ?? throw new ArgumentNullException(nameof(redocOptions));
             this.wikiHtmlSection = new Lazy<string>(BuildWikiHtmlSection);
             this.markdownHandler = markdownHandler ?? throw new ArgumentNullException(nameof(markdownHandler));
+
+            switch (this.redocOptions.WikiNavigationMode)
+            {
+                case RedocNavigationMode.FoldersAsTagGroupsFilesAsTags:
+                    this.idSlug = "tag";
+                    break;
+
+                case RedocNavigationMode.FoldersAsTagsFilesAsHeadings:
+                    this.idSlug = "section";
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         public string GetWikiHtmlSection()
@@ -55,7 +72,7 @@ namespace Marsman.ReallySimpleDocumentation
                         // No nav item for the same-name file, and always put the file at the top of the folder section.
                         var html = Markdown.ToHtml(file.Content, mp);
                         html = InsertHeadingTagIds(swaggerNav, file, html, 2);
-                        swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", file.Name).Replace("{{content}}", html));
+                        swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", file.Name).Replace("{{ref-type}}", idSlug).Replace("{{content}}", html));
                     }
                     foreach (var file in folder.Where(x => x.Name != folder.Name))
                     {
@@ -63,11 +80,11 @@ namespace Marsman.ReallySimpleDocumentation
                         navCount++;
                         swaggerNav.AppendLine(options.NavBarItemTemplate
                                                      .Replace("{{name}}", file.Name)
-                                                     .Replace("{{ref-type}}", "tag")
+                                                     .Replace("{{ref-type}}", idSlug)
                                                      .Replace("{{nav-bar-item-wrapper-class}}", " nav-level-1"));
 
                         html = InsertHeadingTagIds(swaggerNav, file, html, 2);
-                        swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", file.Name).Replace("{{content}}", html));
+                        swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", file.Name).Replace("{{ref-type}}", idSlug).Replace("{{content}}", html));
                     }
                 }
                 
@@ -75,7 +92,7 @@ namespace Marsman.ReallySimpleDocumentation
                 {
                     var html = Markdown.ToHtml(rootFile.Content, mp);
                     html = InsertHeadingTagIds(swaggerNav, rootFile, html, 1);
-                    swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", rootFile.Name).Replace("{{content}}", html));
+                    swaggerContent.AppendLine(options.ContentTemplate.Replace("{{name}}", rootFile.Name).Replace("{{ref-type}}", idSlug).Replace("{{content}}", html));
                 }
             }
 
@@ -100,10 +117,10 @@ namespace Marsman.ReallySimpleDocumentation
             foreach (var heading in headings.Where(x => x.Level == 1 || x.Level == 2))
             {
                 var text = heading.Inline.FirstChild.ToString();
-                var section = $"section/{text}";
+                var id = $"section/{text}";
                 html = Regex.Replace(html,
                                      @$"<(h[1-2])>{Regex.Escape(text)}</\1>",
-                                     $@"<$1 id='{section}'>{text}</$1>",
+                                     $@"<$1 id='{id}'>{text}</$1>",
                                      RegexOptions.IgnoreCase);
 
                 swaggerNav.AppendLine(options.NavBarItemTemplate
